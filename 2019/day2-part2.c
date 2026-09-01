a2d8aee4c4ea7719c37f17229dfa85f92b5ba2c1
ctelvers <charlotte.elvers@tunbury.org>
2026-09-01 13:48:16 +0100

    Create day2-part2.c

---
+++ 2019/day2-part2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char** read_file(char* name, int* lc) {
+	FILE* input = fopen(name,"r");
+	fseek(input, 0L, SEEK_END);
+	long length = ftell(input) + 1;
+	fseek(input, 0L, SEEK_SET);
+	char* buffer  = malloc(length);
+	int n = fread(buffer, sizeof(char), length, input);
+	fclose(input);
+	buffer[n] = '\0';
+	int line_count = 1;
+	for (long i = 0; i < n; i++) {
+		if (buffer[i]== ',') {
+			line_count++;
+		}
+	}
+	*lc = line_count;
+	char** lines = malloc(line_count * sizeof (*lines));
+	int j = 0;
+	lines[j] = buffer;
+	j++;
+	for (int i = 0; i<n; i++) {
+		if (buffer[i] == ',') {
+			buffer[i] = 0;
+			if (i+ 1 < n) lines [j++] = & buffer[i + 1];
+		}
+	}
+	return lines;
+}
+
+int main()
+{
+	int size = 12;
+	char** line = read_file("day2-2019-input.txt", &size);
+	int i;
+	int* memory = malloc(size * sizeof(int));
+	for (int noun = 0; noun < 99; noun++) {
+		for (int verb = 0; verb < 99; verb++) {
+			for (i = 0; i < size; i++) {
+				memory[i] = atoi(line[i]);
+			}
+			memory[1] = noun;
+			memory[2] = verb;
+			int ip = 0;
+			while (memory[ip] != 99) {
+/*				for (i = 0; i < size; i++) {
+					printf("%i,", memory[i]);
+				}
+*/			//	printf("\n");
+				if (memory[ip] == 1) {
+					int num1 = memory[memory[ip + 1]];
+					int num2 = memory[memory[ip + 2]];
+					int res = num1 + num2;
+					memory[memory[ip + 3]] = res;
+					ip += 4;
+//					printf("Result = %i\n", res);
+				}
+				if (memory[ip] == 2) {
+					int num1 = memory[memory[ip + 1]];
+					int num2 = memory[memory[ip + 2]];
+					int res = num1 * num2;
+					memory[memory[ip + 3]] = res;
+					ip += 4;
+//					printf("Result = %i\n", res);
+				}
+			}
+			if(memory[0] == 19690720)
+				printf("the answer is :%i\n", 100 * noun + verb);
+		}
+	}
+//	printf("%i\n", memory[0]);
+	return 0;
+}
